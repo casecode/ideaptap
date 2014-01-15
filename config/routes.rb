c912e6876f4b29296d64d75a76afa77c2305f47e
Laney <laney.bevill@gmail.com>
2014-01-15 13:02:21 -0600

    add devise reg controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Ideatap::Application.routes.draw do
 
   root 'static_files#landing'
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+ 
   resources :users do
     resources :transactions
   end
