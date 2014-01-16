217159116498065d0ebed21fbdcd03cc878e966f
MakerSquare Student <students@themakersquare.com>
2014-01-15 23:16:15 -0600

    add js for idea wall presenter

---
+++ app/assets/javascripts/app/presenters/idea-wall-presenter.js
@@ -0,0 +1,16 @@
+(function () {
+
+  window.IdeaWall = function (options) {
+
+    var ideaTemplate = $('#templates .idea').html();
+    var $root = options.root;
+    var ideas = options.ideas;
+
+    ideas.on('create', function (newIdea) {
+      var newHtml = $.render(ideaTemplate, newIdea);
+      $root.append(newHtml);
+    });
+  };
+
+
+})();
