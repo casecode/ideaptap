3e245c05cf3be85d4ee45420e4a42e517fb8fed4
MakerSquare Student <students@themakersquare.com>
2014-01-16 16:21:59 -0600

    initialize user page with persisted ideas from database

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,17 @@ class Idea < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
   has_many :comments
+
+  def as_json(options={})
+    {
+      id: self.id,
+      title: self.title,
+      category: self.category,
+      short_desc: self.short_desc,
+      long_desc: self.long_desc,
+      idea_support_url: self.idea_support_url,
+      coffer: self.coffer,
+      user_id: self.user_id
+    }
+  end
 end
