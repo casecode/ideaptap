71b0a29362373fa5118a8bce9fb5ab97073d6c74
Laney <laney.bevill@gmail.com>
2014-01-25 13:33:57 -0600

    org show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,8 @@ module UsersHelper
   def my_ideas_most_recent_first
     current_user.ideas.sort{ |a,b| b.created_at <=> a.created_at }
   end
+
+  def my_org_ideas_by_coffer(category)
+  	current_user.organization.ideas.find(:all, :conditions => {:category => "#{category}"}).sort_by(&:coffer).reverse.first(5)
+  end
 end
