e6c45be8c4ce05b211feefd69d13ccafffd76736
Laney <laney.bevill@gmail.com>
2014-01-25 13:59:30 -0600

    minor edits to org page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,6 +9,6 @@ module UsersHelper
   end
 
   def my_org_ideas_by_coffer(category)
-  	current_user.organization.ideas.find(:all, :conditions => {:category => "#{category}"}).sort_by(&:coffer).reverse.first(5)
+  	current_user.organization.ideas.find(:all, :conditions => {:category => "#{category}"}).sort_by(&:coffer).reverse
   end
 end
